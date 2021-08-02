944865383d05797671364060ee1f931cf0871127
Meet-forever <mpatel195@student.gsu.edu>
2021-08-02 01:44:23 -0400

    visual and logical updates

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,52 @@
-
-export default function Form({inputText, setText, textArr, setArr, setCondition}) {
-    const InputHandler = (e) =>{
-        setText(e.target.value);
-    };
-    const SubmitHandler = (e) =>{
-        e.preventDefault();
-        setArr(i =>[
+export default function Form({
+  inputText,
+  setText,
+  textArr,
+  setArr,
+  setCondition,
+}) {
+  const InputHandler = (e) => {
+      setText(e.target.value);
+    
+  };
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    if(inputText != ""){
+        setArr((i) => [
             ...textArr,
             {
-                userString: inputText,
-                status: false,
-                id: Math.random() * 1000
+              userString: inputText,
+              status: false,
+              id: Math.random() * 1000
             }
-        ]);
-        setText("");
-    };
-    const OptionHandler = (e) =>{
-        setCondition(e.target.value);
-    }; 
-    return (
-        <form>
-            <div>
-                <input className="inputTextArea" value={inputText} type="text" onInput={InputHandler} />
-                <button className="submit-btn" type="submit" onClick={SubmitHandler}>Submit</button>
-            </div>
-            <select className="selectBox" onChange={OptionHandler}>
-                <option value="all" >ALL</option>
-                <option value="completed" >Completed</option>
-                <option value="remaining" >Remaining</option>
-            </select>    
-        </form>
-    )
+          ]);
+    }
+    setText("");
+  };
+  const OptionHandler = (e) => {
+    setCondition(e.target.value);
+  };
+  return (
+    <form>
+      <div>
+        <input
+          className="inputTextArea"
+          value={inputText}
+          type="text"
+          onInput={InputHandler}
+        />
+        <button className="submit-btn" type="submit" onClick={SubmitHandler}>
+          Submit
+        </button>
+      </div>
+      <select className="selectBox" onChange={OptionHandler}>
+        <option value="all">ALL</option>
+        <option value="completed">Completed</option>
+        <option value="remaining">Remaining</option>
+      </select>
+    </form>
+  );
 }
-
 
 // <input type="radio" id="finished" name="options" value="finished" onClick={CheckHandler}/>
 // <label htmlFor="finished">Completed</label>
